b2dcccffd7ebbeddc7f2859b3bfb5f209568847b
lirish1973 <ofnoacomps@gmail.com>
2025-10-02 21:54:18 +0300

    הוספת אפשרות צפייה בכל הצעות המחיר

---
+++ TmsSystem/ViewModels/ShoeOfferViewModel.cs
@@ -0,0 +1,12 @@
+﻿using TmsSystem.Models;
+
+namespace TmsSystem.ViewModels
+{
+    public class ShowOfferViewModel
+    {
+        public Offer Offer { get; set; }
+      //  public PaymentMethod PaymentMethod { get; set; }
+        public PaymentMethod? PaymentMethod { get; set; }
+        public int? PaymentMethodId { get; set; } // הוסף ? להפוך ל-nullable
+    }
+}
